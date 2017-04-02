75871aa8fd775105733fea2d3edfe327318a4949
Samuelson <samuelsonma@gmail.com>
2017-04-02 15:21:31 -0400

    criando webcomponets

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,47 @@
 <template>
-  <div>
-    <h1>{{titulo}}</h1>
-    <img :src="foto.url" :alt="foto.titulo">
+  <div class="corpo">
+    <h1 class="centralizado">{{titulo}}</h1>
+    
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos">
+
+      <meu-painel :titulo="foto.titulo">
+
+          <img class="imagem-resposiva" :src="foto.url" :alt="foto.titulo">
+      
+      <meu-painel>
+
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import Painel from './components/shared/painel/Painel.vue';
+
 export default {
+
+  components: {
+    'meu-painel': Painel
+  },
 
   data(){
     return {
       titulo: 'VueGallery',
-      foto: {
-        url: 'http://sensoincomum.org/wp-content/uploads/2016/04/jairbolsonaro-1.jpg',
-        titulo: 'Presidente da República'
-      }
+      fotos: []
     }
+  },
+
+  created(){
+    this.$http.get('http://localhost:3000/v1/fotos')
+    .then(res => res.json())
+    .then(fotos => this.fotos = fotos, err => console.log(err));
+    
+    /*
+    promise.then(res => {
+      res.json().then(fotos => this.fotos = fotos);
+    });
+    */
   }
   
 }
@@ -23,4 +49,27 @@ export default {
 
 <style>
 
+  .centralizado {
+    text-align: center;
+  }
+
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .imagem-resposiva{
+    width: 100%;
+  }
+
+ 
 </style>
