d9a8312574abd0b378e8d85b1329c793d669767f
Samuelson <samuelsonma@gmail.com>
2017-04-02 22:32:38 -0400

    divindo e melhorando SPA

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,31 @@
 <template>
   <div class="corpo">
+
+  <nav>
+    <ul>
+      <li v-for="route in routes">
+        <router-link :to="route.path ? route.path : '/'">{{ route.titulo }}</router-link>
+      </li>
+    </ul>
+  </nav>
+
+<transition name="pagina-fade">
     <router-view></router-view>
+ </transition>   
   </div>
 </template>
 
 <script>
 
+import { routes } from './routes';
 
 export default {
 
+  data(){
+    return {
+      routes
+    }
+  }
   
 }
 </script>
@@ -19,6 +36,14 @@ export default {
     font-family: Helvetica, sans-serif;
     margin: 0 auto;
     width: 96%;
+  }
+
+  .pagina-fade-enter, .pagina-fade-leave-active{
+    opacity: 0;
+  }
+
+  .pagina-fade-enter-active, .pagina-fade-leave-active{
+    transition: opacity .4s;
   }
 
 </style>
